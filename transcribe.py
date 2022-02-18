5b0f3a7d728743d9f525b42d07acb38edab608ea
Victor Galbraith <37874205+vgalbraith@users.noreply.github.com>
2022-02-18 12:58:39 -0800

    Adjusted timeout on transcribe() method to support longer audio files, outputs the current time after each step.

--- transcribe.py
+++ transcribe.py
@@ -1,6 +1,6 @@
 # Copyright 2021 Google Inc. All Rights Reserved:
 #   upload_blob()
-#   transcribe_gcs()
+#   transcribe()
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,24 +16,24 @@
 batch processing.
 """
 
-
 import argparse
 import glob
 import os
 import sys
+import time
 
 from google.cloud import speech
 from google.cloud import storage
 from pathlib import Path
 from pydub import AudioSegment
 
+bucket_name = "reelradio"
 
 # [START convert .m4a to .flac]
 def convert_m4a_to_flac(m4a_file):
-    print("Waiting for conversion to FLAC...")
     new_version = AudioSegment.from_file(m4a_file)
-    new_version.export(Path(m4a_file).stem + ".flac", format="flac", parameters=["-ar", "16000", "-ac", "1"])
-    print("Conversion complete...")
+    new_version.export(Path(m4a_file).stem + ".flac", format="flac", parameters=["-ac", "1", "-ar", "16000"])
+    print(time.ctime() + " - Conversion complete.\n")
 # [END convert .m4a to .flac]
 
 
@@ -51,15 +51,14 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
 
-    print("Waiting for upload to Google Cloud Storage...")
     blob.upload_from_filename(source_file_name)
 
-    print("Upload complete...")
+    print(time.ctime() + " - Upload complete.\n")
 # [END storage_upload_file]
 
 
 # [START speech_transcribe_async_gcs]
-def transcribe_gcs(gcs_uri):
+def transcribe(gcs_uri):
     """Asynchronously transcribes the audio file specified by the gcs_uri."""
     client = speech.SpeechClient()
 
@@ -71,19 +70,17 @@ def transcribe_gcs(gcs_uri):
     )
 
     operation = client.long_running_recognize(config=config, audio=audio)
-
-    print("Waiting for transcript to complete...")
-    response = operation.result(timeout=90)
+    response = operation.result(timeout=5000)
 
     # Each result is for a consecutive portion of the audio. Iterate through
     # them to get the transcripts for the entire audio file.
     for result in response.results:
         # The first alternative is the most likely one for this portion.
-        # print(u"Transcript: {}".format(result.alternatives[0].transcript))
         f.write(u"{}".format(result.alternatives[0].transcript))
         print("Confidence: {}".format(result.alternatives[0].confidence))
 
     f.close()
+    print(time.ctime() + " - Transcription complete.\n")
 # [END speech_transcribe_async_gcs]
 
 
@@ -91,15 +88,18 @@ if __name__ == "__main__":
     # grab stem from filename open new file named {filename}.txt
     audio_name = sys.argv[1]
     audio_stem = Path(audio_name).stem
+    audio_converted = audio_stem + ".flac"
     audio_text = audio_stem + ".txt"
     f = open(audio_text, "a")
 
     # Convert audio
+    print(time.ctime() + " - Converting " + audio_stem + ".m4a to " + audio_converted + " ...")
     convert_m4a_to_flac(audio_name)
-    audio_converted = audio_stem + ".flac"
 
     # Upload converted audio file
-    upload_blob("reelradio", audio_converted, audio_converted)
+    print(time.ctime() + " - Uploading " + audio_converted + " to Google Cloud Storage ...")
+    upload_blob(bucket_name, audio_converted, audio_converted)
 
     # Transcribe audio file
-    transcribe_gcs("gs://reelradio/" + audio_converted)
+    print(time.ctime() + " - Transcribing " + audio_converted + " to " + audio_text + " ...")
+    transcribe("gs://" + bucket_name + "/" + audio_converted)
